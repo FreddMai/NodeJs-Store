bdddbe39bdc16abdb1961af31bb3d9a269229dc3
Freddy M <fredtein@gmail.com>
2020-03-27 23:05:41 +0300

    Adding relations using sequelize and cart module

--- controllers/admin.js
+++ controllers/admin.js
@@ -13,15 +13,16 @@ exports.postAddProduct = (req, res, next) => {
   const imageUrl = req.body.imageUrl;
   const price = req.body.price;
   const description = req.body.description;
-  Product.create({
-    title: title,
-    price: price,
-    imageUrl: imageUrl,
-    description: description
-  })
+  req.user
+    .createProduct({
+      title: title,
+      price: price,
+      imageUrl: imageUrl,
+      description: description
+    })
     .then(result => {
       console.log("Created Product");
-      res.redirect('/admin/products');
+      res.redirect("/admin/products");
     })
     .catch(err => console.log(err));
 };
@@ -32,8 +33,11 @@ exports.getEditProduct = (req, res, next) => {
     return res.redirect("/");
   }
   const prodId = req.params.productId;
-  Product.findByPk(prodId)
-    .then(product => {
+  req.user
+    .getProducts({ where: { id: prodId } })
+    // Product.findByPk(prodId)
+    .then(products => {
+      const product = products[0];
       if (!product) {
         return res.redirect("/");
       }
@@ -69,7 +73,8 @@ exports.postEditProduct = (req, res, next) => {
 };
 
 exports.getProducts = (req, res, next) => {
-  Product.findAll()
+  req.user
+    .getProducts()
     .then(products => {
       res.render("admin/products", {
         prods: products,
@@ -89,7 +94,7 @@ exports.postDeleteProduct = (req, res, next) => {
       return product.destroy();
     })
     .then(result => {
-      console.log('DESTROYED PRODUCT');
+      console.log("DESTROYED PRODUCT");
       res.redirect("/admin/products");
     })
     .catch(err => console.log(err));
