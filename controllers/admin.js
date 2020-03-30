4219db4bb0c168235e0cca9d21abea328b1e9180
Freddy M <fredtein@gmail.com>
2020-03-30 13:28:44 +0300

    Playing with cookies

--- controllers/admin.js
+++ controllers/admin.js
@@ -4,7 +4,8 @@ exports.getAddProduct = (req, res, next) => {
   res.render("admin/edit-product", {
     pageTitle: "Add Product",
     path: "/admin/add-product",
-    editing: false
+    editing: false,
+    isAuth: req.isLoggedIn
   });
 };
 
@@ -43,7 +44,8 @@ exports.getEditProduct = (req, res, next) => {
         pageTitle: "Edit Product",
         path: "/admin/edit-product",
         editing: editMode,
-        product: product
+        product: product,
+        isAuth: req.isLoggedIn
       });
     })
     .catch(err => console.log(err));
@@ -76,7 +78,8 @@ exports.getProducts = (req, res, next) => {
       res.render("admin/products", {
         prods: products,
         pageTitle: "Admin Products",
-        path: "/admin/products"
+        path: "/admin/products",
+        isAuth: req.isLoggedIn
       });
     })
     .catch(err => {
--- controllers/auth.js
+++ controllers/auth.js
@@ -1,6 +1,17 @@
 exports.getLogin = (req, res, next) => {
+  const isLoggedIn = req
+    .get("Cookie")
+    .split(";")[0]
+    .trim()
+    .split("=")[1] == 'true';
   res.render("auth/login", {
     path: "/login",
-    pageTitle: "Login"
+    pageTitle: "Login",
+    isAuth: isLoggedIn
   });
+};
+
+exports.postLogin = (req, res, next) => {
+  res.setHeader("Set-Cookie", "loggedIn=true");
+  res.redirect("/");
 };
