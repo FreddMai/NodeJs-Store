32f5891233d0b501425e555c1360d89fc77fc852
Freddy M <fredtein@gmail.com>
2020-03-31 16:24:21 +0300

    Using sendgrid for signup

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,6 +1,14 @@
 const bcrypt = require("bcryptjs");
+const nodemailer = require('nodemailer');
+const sendGridTransport = require('nodemailer-sendgrid-transport');
 
 const User = require("../models/user");
+
+const transporter = nodemailer.createTransport(sendGridTransport({
+  auth: {
+    api_key: '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'
+  }
+}));
 
 exports.getLogin = (req, res, next) => {
   let message = req.flash('error');
@@ -83,6 +91,14 @@ exports.postSignup = (req, res, next) => {
         })
         .then(result => {
           res.redirect("/login");
+          return transporter.sendMail({
+            to: email,
+            from: 'shop@node-shop.com',
+            subject: 'Signup succeeded!',
+            html: '<h1>You successfully signed up!</h1>'
+          });
+        }).catch(err => {
+          console.log(err);
         });
     })
     .catch(err => {
