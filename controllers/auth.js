fc99c5d721ad15f2addeaac866e7b5fd7e3f99bf
Freddy M <fredtein@gmail.com>
2020-03-31 12:28:51 +0300

    Some fixes to code and css

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,4 +1,4 @@
-const User = require('../models/user');
+const User = require("../models/user");
 
 exports.getLogin = (req, res, next) => {
   res.render("auth/login", {
@@ -9,17 +9,19 @@ exports.getLogin = (req, res, next) => {
 };
 
 exports.postLogin = (req, res, next) => {
-    User.findById("5e819a1f4a59840b40074b10")
-      .then(user => {
-        req.session.isLoggedIn = true;
-        req.session.user = user;
+  User.findById("5e819a1f4a59840b40074b10")
+    .then(user => {
+      req.session.isLoggedIn = true;
+      req.session.user = user;
+      req.session.save(() => {
         res.redirect("/");
-      })
-      .catch(err => console.log(err));
+      });
+    })
+    .catch(err => console.log(err));
 };
 
 exports.postLogout = (req, res, next) => {
-  req.session.destroy((err) => {
-     res.redirect('/');
+  req.session.destroy(err => {
+    res.redirect("/");
   });
 };
