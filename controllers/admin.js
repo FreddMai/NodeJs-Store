cf9adf1c77cc860bf0db7beef5aa8210715855bd
Freddy M <fredtein@gmail.com>
2020-03-31 11:43:39 +0300

    Using sessions

--- controllers/admin.js
+++ controllers/admin.js
@@ -5,7 +5,7 @@ exports.getAddProduct = (req, res, next) => {
     pageTitle: "Add Product",
     path: "/admin/add-product",
     editing: false,
-    isAuth: req.isLoggedIn
+    isAuth: req.session.isLoggedIn
   });
 };
 
@@ -19,7 +19,7 @@ exports.postAddProduct = (req, res, next) => {
     price: price,
     description: description,
     imageUrl: imageUrl,
-    userId: req.user
+    userId: req.session.user
   });
   product
     .save()
@@ -45,7 +45,7 @@ exports.getEditProduct = (req, res, next) => {
         path: "/admin/edit-product",
         editing: editMode,
         product: product,
-        isAuth: req.isLoggedIn
+        isAuth: req.session.isLoggedIn
       });
     })
     .catch(err => console.log(err));
@@ -79,7 +79,7 @@ exports.getProducts = (req, res, next) => {
         prods: products,
         pageTitle: "Admin Products",
         path: "/admin/products",
-        isAuth: req.isLoggedIn
+        isAuth: req.session.isLoggedIn
       });
     })
     .catch(err => {
--- controllers/auth.js
+++ controllers/auth.js
@@ -1,17 +1,25 @@
+const User = require('../models/user');
+
 exports.getLogin = (req, res, next) => {
-  const isLoggedIn = req
-    .get("Cookie")
-    .split(";")[0]
-    .trim()
-    .split("=")[1] == 'true';
   res.render("auth/login", {
     path: "/login",
     pageTitle: "Login",
-    isAuth: isLoggedIn
+    isAuth: false
   });
 };
 
 exports.postLogin = (req, res, next) => {
-  res.setHeader("Set-Cookie", "loggedIn=true");
-  res.redirect("/");
+    User.findById("5e819a1f4a59840b40074b10")
+      .then(user => {
+        req.session.isLoggedIn = true;
+        req.session.user = user;
+        res.redirect("/");
+      })
+      .catch(err => console.log(err));
+};
+
+exports.postLogout = (req, res, next) => {
+  req.session.destroy((err) => {
+     res.redirect('/');
+  });
 };
