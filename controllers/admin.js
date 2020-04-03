e9386b13025f025bfd5ae4aee2cd9547d3371c85
Freddy M <fredtein@gmail.com>
2020-04-03 12:08:23 +0300

    Adding file upload

--- controllers/admin.js
+++ controllers/admin.js
@@ -14,9 +14,26 @@ exports.getAddProduct = (req, res, next) => {
 
 exports.postAddProduct = (req, res, next) => {
   const title = req.body.title;
-  const imageUrl = req.body.imageUrl;
+  const imageUrl = req.file;
   const price = req.body.price;
   const description = req.body.description;
+
+  if (!image) {
+    return res.status(422).render('admin/edit-product', {
+      pageTitle: 'Add Product',
+      path: '/admin/add-product',
+      editing: false,
+      hasError: true,
+      product: {
+        title: title,
+        price: price,
+        description: description
+      },
+      errorMessage: 'Attached file is not an image.',
+      validationErrors: []
+    });
+  }
+  
   const errors = validationResult(req);
 
   if (!errors.isEmpty()) {
